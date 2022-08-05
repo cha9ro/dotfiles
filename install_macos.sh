ac6e0271be03d4ed41f6c1aa3075bf219cd7de31
Takuro Takezawa <t910inicident@gmail.com>
2022-08-06 00:26:01 +0900

    :bug: fix bugs in install script

--- install_macos.sh
+++ install_macos.sh
@@ -16,13 +16,13 @@ fi
 # initialize anyenv: https://github.com/anyenv/anyenv
 # add update plugin: https://github.com/znz/anyenv-update
 if [ ! -d ${HOME}/.config/anyenv/anyenv-install ]; then
-    ${HOME}/.anyenv/bin/anyenv init
-    mkdir -p $(${HOME}/.anyenv/bin/anyenv root)/plugins
-    git clone https://github.com/znz/anyenv-update.git $(${HOME}/.anyenv/bin/anyenv root)/plugins/anyenv-update
+    anyenv install --init
+    mkdir -p $(anyenv root)/plugins
+    git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
 fi
 
 # install Vundle: https://github.com/VundleVim/Vundle.vim
-fi [ ! -d ${HOME}/.vim/bundle ]; then
+if [ ! -d ${HOME}/.vim/bundle ]; then
     mkdir -p ${HOME}/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 fi
