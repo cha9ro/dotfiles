97be5bb162485b3dafdd59d43bd27d5b558c9435
Takuro Takezawa <t910inicident@gmail.com>
2025-07-29 17:37:12 +0900

    regular update

--- .p10k.zsh
+++ .p10k.zsh
@@ -990,7 +990,7 @@
   # Show node version only when in a directory tree containing package.json.
   typeset -g POWERLEVEL9K_NODE_VERSION_PROJECT_ONLY=true
   # Custom icon.
-  typeset -g POWERLEVEL9K_NODE_VERSION_VISUAL_IDENTIFIER_EXPANSION='󰎙'
+  typeset -g POWERLEVEL9K_NODE_VERSION_VISUAL_IDENTIFIER_EXPANSION=$'\uED0D'
 
 
   #######################[ go_version: go version (https://golang.org) ]########################
