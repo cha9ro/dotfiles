a19956cbc65c9c3817289cf3e755231c6e7bbb7b
Takuro Takezawa <t910inicident@gmail.com>
2025-07-31 11:26:48 +0900

    regular update
    - update install script
    - iterm appearance
    - format vscode settings
    - update Brewfile

--- install_macos.sh
+++ install_macos.sh
@@ -7,27 +7,13 @@ fi
 # install formulae by Homebrew
 brew bundle
 
-# install sdkman: https://sdkman.io/install
-if [ ! -d ${HOME}/.sdkman ]; then
-    curl -s "https://get.sdkman.io" | bash
-    source ${HOME}/.sdkman/bin/sdkman-init.sh
-fi
-
-# initialize anyenv: https://github.com/anyenv/anyenv
-# add update plugin: https://github.com/znz/anyenv-update
-if [ ! -d ${HOME}/.config/anyenv/anyenv-install ]; then
-    anyenv install --init
-    mkdir -p $(anyenv root)/plugins
-    git clone https://github.com/znz/anyenv-update.git $(anyenv root)/plugins/anyenv-update
-fi
-
 # install Vundle: https://github.com/VundleVim/Vundle.vim
 if [ ! -d ${HOME}/.vim/bundle ]; then
     mkdir -p ${HOME}/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 fi
 
-# make symbolic links
+# make symbolic links for dotfiles
 dotfiles=(
     ".vimrc"
     ".zshrc"
@@ -40,4 +26,14 @@ dotfiles=(
 for file in ${dotfiles[@]}
 do
     ln -fs ${PWD}/${file} ${HOME}/$file
+done
+
+# make symbolic links for vscode settings
+vscode_settings=(
+    "vscode/settings.json"
+    "vscode/keybindings.json"
+)
+for file in ${vscode_settings[@]}
+do
+    ln -fs ${PWD}/${file} ${HOME}/Library/Application\ Support/Code/User/$file
 done
