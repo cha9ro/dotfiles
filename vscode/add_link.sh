43b4c6ffd2945b8a5f9b7df8d44e3214baec6c86
Takuro Takezawa <t910inicident@gmail.com>
2026-02-05 23:31:00 +0900

    refactor brewfiles

--- vscode/add_link.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-VSCODE_USER_DIR="${HOME}/Library/Application Support/Code/User"
-
-ln -s ${HOME}/dotfiles/vscode/settings.json ${VSCODE_USER_DIR}/settings.json
-ln -s ${HOME}/dotfiles/vscode/keybindings.json ${VSCODE_USER_DIR}/keybindings.json
